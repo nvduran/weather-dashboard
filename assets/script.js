7b073ee5e7e254686743f3d74f386dd356f0c6cf
Nick Duran <nvduran@gmail.com>
2021-02-14 16:42:14 -0600

    divs and weather call

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
+var cityName = "Austin";
+
 console.log("test");
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7718d66c5dd4f433e079cf4a982af6f7";
+
+fetch(apiUrl).then(function(response){
+     response.json().then(function(data){
+          console.log(data);
+     });
+});
