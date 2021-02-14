2d970dc72c9722b433b088973686e2065a7353a3
Nick Duran <nvduran@gmail.com>
2021-02-14 17:38:35 -0600

    weather api working with search

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
+var cityForm = document.querySelector("#city-form");
 var cityName = "Austin";
+//city search button
+$("#stateBtn").on('click', function(){
+     var cityChoice = cityForm.value.trim();
+     console.log(cityChoice);
 
-console.log("test");
-
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7718d66c5dd4f433e079cf4a982af6f7";
-
-fetch(apiUrl).then(function(response){
-     response.json().then(function(data){
-          console.log(data);
-     });
+     weatherFetch(cityChoice);
 });
+
+//pull weather data for chosen city
+var weatherFetch = function(city){
+     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7718d66c5dd4f433e079cf4a982af6f7";
+     fetch(apiUrl).then(function(response){
+          response.json().then(function(data){
+               console.log(data);
+          });
+     });
+};
+
