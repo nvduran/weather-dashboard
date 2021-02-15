a87fe1d34719129e2ca16d37af79859811424cd7
Nick Duran <nvduran@gmail.com>
2021-02-14 20:54:52 -0600

    uv index pull

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,52 @@
 var cityForm = document.querySelector("#city-form");
-var cityName = "Austin";
+
 //city search button
 $("#stateBtn").on('click', function(){
      var cityChoice = cityForm.value.trim();
      console.log(cityChoice);
-
      weatherFetch(cityChoice);
 });
 
 //pull weather data for chosen city
 var weatherFetch = function(city){
-     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7718d66c5dd4f433e079cf4a982af6f7";
+     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=7718d66c5dd4f433e079cf4a982af6f7";
      fetch(apiUrl).then(function(response){
-          response.json().then(function(data){
-               console.log(data);
+          response.json().then(function(data){              
+               addinfo(data);
           });
      });
 };
 
+//add weather to right side div
+var addinfo = function(data){    
+     console.log(data);   
+     let cityName = $("<div>").text(data.name).addClass('h1');
+     let cityTemp = $("<div>").text("Temperature: " + data.main.temp + "°F");
+     let cityHumidity = $("<div>").text("Humidity: " + data.main.humidity + "%");
+     let cityWind = $("<div>").text("Wind Speed: " + data.wind.speed + " MPH");
+
+     $("#weather-div").append(cityName);
+     $("#weather-div").append(cityTemp);
+     $("#weather-div").append(cityHumidity);
+     $("#weather-div").append(cityWind);
+     getUv(data);
+};
+
+//get UV index
+var getUv = function(data){
+     var cityLat = data.coord.lat.toFixed(2);
+     var cityLon = data.coord.lon.toFixed(2);
+     var uvApi = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=7718d66c5dd4f433e079cf4a982af6f7";
+     console.log(uvApi);
+     fetch(uvApi).then(function(response){
+          response.json().then(function(uvData){
+               console.log(uvData.value);
+               addUv(uvData.value);
+          });         
+     });
+};
+
+//add UV index to page
+var addUv = function(value){
+     console.log(value);
+};
