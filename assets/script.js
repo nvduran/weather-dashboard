4a372325662dcf18f88dd59a4f1d98983cd0dd16
Nick Duran <nvduran@gmail.com>
2021-02-15 14:37:20 -0600

    history

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var cityForm = document.querySelector("#city-form");
+$(".card").hide();
 
 //city search button
 $("#stateBtn").on('click', function(){
@@ -31,6 +32,7 @@ var addinfo = function(data){
      $("#weather-div").append(cityWind);
      getUv(data);
      forecast(data);
+     saveSearch(data);
 };
 
 //get UV index
@@ -59,22 +61,59 @@ var addUv = function(value){
           $(cityUv).addClass('uv-high');
      };
 };
-
+//get forecast from api
 var forecast = function(data){
      var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + data.name + "&units=imperial&appid=7718d66c5dd4f433e079cf4a982af6f7";
      console.log(forecastApi);
      fetch(forecastApi).then(function(response){
           response.json().then(function(castData){
                console.log(castData);
+               console.log(castData.list[0]);
+               castCard(castData);
           });
      });
 };
+//add forecast data to cards
+var castCard = function(castData){
+     $("#header1").text(castData.list[0].dt_txt);
+     $("#header2").text(castData.list[8].dt_txt);
+     $("#header3").text(castData.list[16].dt_txt);
+     $("#header4").text(castData.list[24].dt_txt);
+     $("#header5").text(castData.list[32].dt_txt);
+     
+     $("#temp1").text("Temperature: " + castData.list[0].main.temp + "°F");
+     $("#temp2").text("Temperature: " + castData.list[8].main.temp + "°F");
+     $("#temp3").text("Temperature: " + castData.list[16].main.temp + "°F");
+     $("#temp4").text("Temperature: " + castData.list[24].main.temp + "°F");
+     $("#temp5").text("Temperature: " + castData.list[32].main.temp + "°F");
 
-var castCard = function(){
-     $("#header1").text('test');
+     $("#hum1").text("Humidity: " + castData.list[0].main.humidity + "%");
+     $("#hum2").text("Humidity: " + castData.list[8].main.humidity + "%");
+     $("#hum3").text("Humidity: " + castData.list[16].main.humidity + "%");
+     $("#hum4").text("Humidity: " + castData.list[24].main.humidity + "%");
+     $("#hum5").text("Humidity: " + castData.list[32].main.humidity + "%");
+     $(".card").show();
 };
 
-castCard();
+var saveSearch = function(data){
+     var i = 0
+     console.log(data.name);
+     localStorage.setItem(i, data.name);
+     i++;
+     
+};
+
+var getSearch = function(){
+     var i;
+     for (i = 0; i < 10; i++) {
+     var savedCity = localStorage.getItem(i);
+     console.log(savedCity);
+     let citySearched = $("<div>").text(savedCity);
+     $("#searches").append(citySearched);
+     };
+};
+
+getSearch();
 // var cardTest = function(){
 //      let testCard = $("<div>").addClass('card');
 //      let cardTitle = $("<div>").text('Header').addClass('card-header');
